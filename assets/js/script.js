35e57f177d148e2e70ba1c7d18de0d755ea38c68
Chandupa <chandupajayalth33@gmail.com>
2024-07-06 09:01:23 +0530

    single product js error fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,18 @@ document.addEventListener("DOMContentLoaded", function() {
     var mainImg = document.getElementById("mainImg");
     var smallImg = document.getElementsByClassName("small-img"); 
 
-    for(let i = 0; i < smallImg.length; i++){
-        smallImg[i].onclick = function(){
-            mainImg.src = smallImg[i].src;
+    if (mainImg && smallImg.length > 0) {
+        for (let i = 0; i < smallImg.length; i++) {
+            smallImg[i].onclick = function() {
+                mainImg.src = smallImg[i].src;
+            }
         }
     }
 
-    document.getElementById("imageProduct").onclick = function () {
-        window.location = "single_product.php";
-    };
+    var imageProduct = document.getElementById("imageProduct");
+    if (imageProduct) {
+        imageProduct.onclick = function() {
+            window.location = "single_product.php";
+        };
+    }
 });
