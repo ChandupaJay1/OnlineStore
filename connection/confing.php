c91c55961fa0080659be2dd47c8dbcada88b663c
Chandupa <chandupajayalth33@gmail.com>
2024-07-06 07:54:18 +0530

    create get product to simplyfy execution of getindex products

--- connection/confing.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$conn = mysqli_connect("localhost", "root", "Chandupa@2022", "onlineshopping_db")
-    or die("couldn't connect to the database");
-    
-?>
--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" href="assets/css/shop.css" />
     <link rel="StyleSheet" href="assets/css/footer.css" />
 
     <link rel="icon" href="assets/images/icon/ico-white.png" />
@@ -22,48 +23,48 @@
     <!-- home -->
 
     <section>
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item active" style="background-image: url('assets/images/img_2.jpg')">
-                <div class="carousel-caption text-center">
-                    <h5>NEW ARRIVALS</h5>
-                    <h1><span>Best Prices</span> this Season</h1>
-                    <p>D26 Clothing offers the best products for the most affordable price</p>
-                    <button class="btn btn-primary">Shop Now</button>
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active" style="background-image: url('assets/images/img_2.jpg')">
+                    <div class="carousel-caption text-center">
+                        <h5>NEW ARRIVALS</h5>
+                        <h1><span>Best Prices</span> this Season</h1>
+                        <p>D26 Clothing offers the best products for the most affordable price</p>
+                        <button class="btn btn-primary">Shop Now</button>
+                    </div>
+                </div>
+                <div class="carousel-item" style="background-image: url('assets/images/img_2.jpg')">
+                    <div class="carousel-caption text-center">
+                        <h5>EXCLUSIVE COLLECTION</h5>
+                        <h1><span>Trendy & Stylish</span> Outfits</h1>
+                        <p>Discover the latest trends in fashion</p>
+                        <button class="btn btn-primary">Explore Now</button>
+                    </div>
+                </div>
+                <div class="carousel-item" style="background-image: url('assets/images/img_2.jpg')">
+                    <div class="carousel-caption text-center">
+                        <h5>SUMMER SALE</h5>
+                        <h1><span>Up to 50% Off</span> on Selected Items</h1>
+                        <p>Don't miss out on these great deals!</p>
+                        <button class="btn btn-primary">Shop Sale</button>
+                    </div>
                 </div>
             </div>
-            <div class="carousel-item" style="background-image: url('assets/images/img_2.jpg')">
-                <div class="carousel-caption text-center">
-                    <h5>EXCLUSIVE COLLECTION</h5>
-                    <h1><span>Trendy & Stylish</span> Outfits</h1>
-                    <p>Discover the latest trends in fashion</p>
-                    <button class="btn btn-primary">Explore Now</button>
-                </div>
-            </div>
-            <div class="carousel-item" style="background-image: url('assets/images/img_2.jpg')">
-                <div class="carousel-caption text-center">
-                    <h5>SUMMER SALE</h5>
-                    <h1><span>Up to 50% Off</span> on Selected Items</h1>
-                    <p>Don't miss out on these great deals!</p>
-                    <button class="btn btn-primary">Shop Sale</button>
-                </div>
-            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>
-</section>
+    </section>
 
 
     <!-- brandings -->
@@ -133,7 +134,7 @@
         </div>
 
         <div class="row mx-auto container-fluid">
-            <?php include('connection/get_featured_product.php'); ?>
+            <?php include('connection/get_products.php'); ?>
 
             <?php while ($row = $featured_product->fetch_assoc()) { ?>
                 <div class="product text-center col-lg-3 col-md-4 col-sm-12" id="singleProduct">
@@ -150,7 +151,7 @@
                     <h5 class="p-name"><?php echo htmlspecialchars($row['product_name']); ?></h5>
                     <h4 class="p-price">RS.<?php echo htmlspecialchars($row['product_price']); ?></h4>
                     <a href="<?php echo 'single_product.php?product_id=' . $row['product_id']; ?>">
-                        <button class="buy-btn btn btn-warning">Buy Now</button>
+                        <button class="buy-btn mt-3">Buy Now</button>
                     </a>
                 </div>
             <?php } ?>
@@ -176,135 +177,126 @@
     <section>
 
         <div class="container text-center py-5">
-            <h3>Dresses & Coats</h3>
+            <h3>Full Suites</h3>
             <hr class="hr mx-auto ">
             <p>Here you can check out our amazing clothes</p>
         </div>
+
         <div class="row mx-auto container-fluid">
+            <?php include('connection/get_products.php'); ?>
 
-            <div class="product text-center col-lg-3 col-md-4 col-sm-12">
-                <div class="image" style="background-image: url('assets/images/clothes/cloth_1.jpg');"></div>
-                <div class="star">
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                </div>
-                <h5 class="p-name"> Adidas T-Shirt</h5>
-                <h4 class="p-price">RS.1200.00</h4>
-                <button class="buy-btn btn btn-warning">Buy Now</button>
-            </div>
+            <?php while ($row = $full_suite->fetch_assoc()) { ?>
+                <div class="product text-center col-lg-3 col-md-4 col-sm-12" id="singleProduct">
+                    <img class="image mb-4" src="assets/images/clothes/<?php echo htmlspecialchars($row['product_image']); ?>" />
 
-            <div class="product text-center col-lg-3 col-md-4 col-sm-12">
-                <div class="image" style="background-image: url('assets/images/clothes/cloth_1.jpg');"></div>
-                <div class="star">
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                </div>
-                <h5 class="p-name"> Adidas T-Shirt</h5>
-                <h4 class="p-price">RS.1200.00</h4>
-                <button class="buy-btn btn btn-warning">Buy Now</button>
-            </div>
+                    <div class="star">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                    </div>
 
-            <div class="product text-center col-lg-3 col-md-4 col-sm-12">
-                <div class="image" style="background-image: url('assets/images/clothes/cloth_1.jpg');"></div>
-                <div class="star">
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
+                    <h5 class="p-name"><?php echo htmlspecialchars($row['product_name']); ?></h5>
+                    <h4 class="p-price">RS.<?php echo htmlspecialchars($row['product_price']); ?></h4>
+                    <a href="<?php echo 'single_product.php?product_id=' . $row['product_id']; ?>">
+                        <button class="buy-btn mt-3">Buy Now</button>
+                    </a>
                 </div>
-                <h5 class="p-name"> Adidas T-Shirt</h5>
-                <h4 class="p-price">RS.1200.00</h4>
-                <button class="buy-btn btn btn-warning">Buy Now</button>
-            </div>
+            <?php } ?>
+        </div>
+    </section>
 
-            <div class="product text-center col-lg-3 col-md-4 col-sm-12">
-                <div class="image" style="background-image: url('assets/images/clothes/cloth_1.jpg');"></div>
-                <div class="star">
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                </div>
-                <h5 class="p-name"> Adidas T-Shirt</h5>
-                <h4 class="p-price">RS.1200.00</h4>
-                <button class="buy-btn btn btn-warning">Buy Now</button>
-            </div>
+    <!-- End Featured -->
+
+    <!-- Caps -->
+
+    <section>
+
+        <div class="container text-center py-5">
+            <h3>Caps</h3>
+            <hr class="hr mx-auto ">
+            <p>Here you can check out our amazing clothes</p>
         </div>
 
         <div class="row mx-auto container-fluid">
 
-            <div class="product text-center col-lg-3 col-md-4 col-sm-12">
-                <div class="image" style="background-image: url('assets/images/clothes/cloth_1.jpg');"></div>
-                <div class="star">
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                </div>
-                <h5 class="p-name"> Adidas T-Shirt</h5>
-                <h4 class="p-price">RS.1200.00</h4>
-                <button class="buy-btn btn btn-warning">Buy Now</button>
-            </div>
+            <?php include('connection/get_products.php'); ?>
 
-            <div class="product text-center col-lg-3 col-md-4 col-sm-12">
-                <div class="image" style="background-image: url('assets/images/clothes/cloth_1.jpg');"></div>
-                <div class="star">
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                </div>
-                <h5 class="p-name"> Adidas T-Shirt</h5>
-                <h4 class="p-price">RS.1200.00</h4>
-                <button class="buy-btn btn btn-warning">Buy Now</button>
-            </div>
+            <?php while ($row = $cap->fetch_assoc()) { ?>
+                <div class="product text-center col-lg-3 col-md-4 col-sm-12" id="singleProduct">
+                    <img class="image mb-4" src="assets/images/clothes/<?php echo htmlspecialchars($row['product_image']); ?>" />
 
-            <div class="product text-center col-lg-3 col-md-4 col-sm-12">
-                <div class="image" style="background-image: url('assets/images/clothes/cloth_1.jpg');"></div>
-                <div class="star">
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                </div>
-                <h5 class="p-name"> Adidas T-Shirt</h5>
-                <h4 class="p-price">RS.1200.00</h4>
-                <button class="buy-btn btn btn-warning">Buy Now</button>
-            </div>
+                    <div class="star">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                    </div>
 
-            <div class="product text-center col-lg-3 col-md-4 col-sm-12">
-                <div class="image" style="background-image: url('assets/images/clothes/cloth_1.jpg');"></div>
-                <div class="star">
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
+                    <h5 class="p-name"><?php echo htmlspecialchars($row['product_name']); ?></h5>
+                    <h4 class="p-price">RS.<?php echo htmlspecialchars($row['product_price']); ?></h4>
+                    <a href="<?php echo 'single_product.php?product_id=' . $row['product_id']; ?>">
+                        <button class="buy-btn mt-3">Buy Now</button>
+                    </a>
                 </div>
-                <h5 class="p-name"> Adidas T-Shirt</h5>
-                <h4 class="p-price">RS.1200.00</h4>
-                <button class="buy-btn btn btn-warning">Buy Now</button>
-            </div>
+            <?php } ?>
+
         </div>
 
     </section>
+
+    <!-- End Caps -->
+
+    <!-- Tshirts -->
+
+    <section>
+
+        <div class="container text-center py-5">
+            <h3>T-Shirts</h3>
+            <hr class="hr mx-auto ">
+            <p>Here you can check out our amazing clothes</p>
+        </div>
+
+        <div class="row mx-auto container-fluid">
+
+            <?php include('connection/get_products.php'); ?>
+
+            <?php while ($row = $tshirt->fetch_assoc()) { ?>
+                <div class="product text-center col-lg-3 col-md-4 col-sm-12" id="singleProduct">
+                    <img class="image mb-4" src="assets/images/clothes/<?php echo htmlspecialchars($row['product_image']); ?>" />
+
+                    <div class="star">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                    </div>
+
+                    <h5 class="p-name"><?php echo htmlspecialchars($row['product_name']); ?></h5>
+                    <h4 class="p-price">RS.<?php echo htmlspecialchars($row['product_price']); ?></h4>
+                    <a href="<?php echo 'single_product.php?product_id=' . $row['product_id']; ?>">
+                        <button class="buy-btn mt-3">Buy Now</button>
+                    </a>
+                </div>
+            <?php } ?>
+
+        </div>
+
+    </section>
+
+    <!-- End Tshirts -->
+
+
+
 
 
     <!-- footer -->
     <?php include('includes/footer-view.php'); ?>
 
-    <script src="assets\js\script.js"></script> -->
+    <script src="assets\js\script.js"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
