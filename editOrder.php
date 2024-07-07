4574943994b55219068fa14428968a6ab909341b
Chandupa <chandupajayalth33@gmail.com>
2024-07-07 22:25:08 +0530

    some changes

--- editOrder.php
+++ editOrder.php
@@ -34,9 +34,10 @@ if (isset($_GET['order_id'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>EDIT ORDERS</title>
+    <title>Edit Orders</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="icon" href="assets/images/icon/ico-white.png" />
 </head>
 
 <body>
